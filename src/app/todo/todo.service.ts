067143fc95a2f6fc9fb785c6d620ad7459dac1d1
fernando <fernandogr060@gmail.com>
2018-01-12 00:33:44 +0000

    chore: Add types and commits to todo.service

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 
+interface Todo {
+  id: number, 
+  title: string, 
+  isDone: boolean
+}
+
 let todos = [
   { id: 0, title: 'Install Angular CLI', isDone: true },
   { id: 1, title: 'Style app', isDone: true },
@@ -8,16 +14,27 @@ let todos = [
   { id: 4, title: 'Break into components', isDone: false },
 ];
 
+
 @Injectable()
 export class TodoService {
 
   constructor() { }
 
-  get(query = ''){
+  /**
+   * Get Todos (all / completed / active)
+   * @param {string} query
+   ** @return {Promise<Array<Todo>>} 
+   */
+  get(query: string = ''): Promise<Array<Todo>> {
     return new Promise(resolve => resolve(this.getByQuery(query)));
   }
 
-  add(newTodo) {
+  /**
+   * Add new Todo
+   * @param {Todo} newTodo
+   * @return {Promise<Todo>} 
+   */
+  add(newTodo: Todo): Promise<Todo> {
     return new Promise(resolve => {
       newTodo.id = todos.length;
       todos.push(newTodo);
@@ -25,15 +42,25 @@ export class TodoService {
     });
   }
 
-  update(data) {
+  /**
+   * Update Todo
+   * @param {Todo} data
+   * @return {Promise<Todo>} 
+   */
+  update(data: Todo): Promise<Todo> {
     return new Promise(resolve => {
       const index = todos.findIndex(todo => todo.id === data.id);
       todos[index].title = data.title;
       resolve(data);
-    })
+    });
   }
 
-  destroy(id) {
+  /**
+   * Delete Todo
+   * @param {number} id
+   * @return {Promise<Boolean>}  
+   */
+  destroy(id: number): Promise<Boolean> {
     return new Promise(resolve => {
       const index = todos.findIndex(todo => todo.id === id);
       todos.splice(index, 1);
@@ -41,39 +68,57 @@ export class TodoService {
     });
   }
 
-  completeTask(id, value) {
-    return new Promise (resolve => {
+  /**
+   * Update isDone of a Todo
+   * @param {number} id 
+   * @param {string} value 
+   * @return {Promise<Array<Todo>>}
+   */
+  completeTask(id: number, value: boolean): Promise<Array<Todo>> {
+    return new Promise(resolve => {
       const index = todos.findIndex(todo => todo.id === id);
       todos[index].isDone = value;
       resolve(todos);
-    })
+    });
   }
 
-  clearAllCompleted(query){
+  /**
+   * Get just the Active (!isDone) Todos
+   * @param {string} query 
+   * @return {Promise<Array<Todo>>}
+   */
+  clearAllCompleted(query: string): Promise<Array<Todo>> {
     return new Promise(resolve => {
       const tasks = this.getByQuery(query);
-
       const toBeDone = tasks.filter(todo => !todo.isDone);
       resolve(toBeDone);
     });
   }
 
-  getByQuery(query) {
+  /**
+   * get Todos based on query
+   * @param {string} query 
+   * @return {Array<Todo>}
+   */
+  getByQuery(query: string): Array<Todo> {
     let data;
-
-    if(query === 'completed' || query === 'active') {
-      console.log(query);
+    if (query === 'completed' || query === 'active') {
       data = this.getActiveOrCompleted(query);
-    } else {
+    }
+    else {
       data = todos;
     }
-    return data
+    return data;
   }
 
-  getActiveOrCompleted(query) {
-      var isCompleted = query === 'completed';
-      const data = todos.filter(todo => todo.isDone === isCompleted);
-
-      return data;
+  /**
+   * Get completed or Active Todos
+   * @param {string} query
+   * @return {Array<Todo>}
+   */
+  getActiveOrCompleted(query: string): Array<Todo> {
+    var isCompleted = query === 'completed';
+    const data = todos.filter(todo => todo.isDone === isCompleted);
+    return data;
   }
 }
